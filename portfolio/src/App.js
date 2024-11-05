859b234445195de7a58f99f36a2de6bb25df9fa4
EunheeJeong1892 <echoeunhee@gmail.com>
2024-11-05 17:14:48 +0000

    fix: change navigation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,17 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Helmet } from "react-helmet-async";
-import Navbar from "./components/Navbar";
+import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Resume from "./components/Resume/Resume";
 import Footer from "./components/Footer";
 import { HelmetProvider } from "react-helmet-async";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -25,14 +20,17 @@ function App() {
           <title>Eunhee Jeong | Portfolio</title>
         </Helmet>
         <div className="App">
-          <Navbar />
+          <NavigationBar />
           <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
+          <section id="home">
+            <Home />
+          </section>
+          <section id="about">
+            <About />
+          </section>
+          <section id="resume">
+            <Resume />
+          </section>
           <Footer />
         </div>
       </Router>
